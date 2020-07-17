fa7c4787ca34240fccbbf09fbd5bd972ce879792
Igor Morgado <morgado.igor@gmail.com>
2020-07-16 23:23:09 -0300

    Chaptere3 done

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 #include <SDL2/SDL.h>
 
 typedef int8_t          i8;
@@ -16,28 +18,79 @@ typedef unsigned int    uint;
 typedef float           f32;
 typedef double          f64;
 
+enum RETURN_STATUS {
+    RETURN_FAILURE = -1,
+    RETURN_SUCCESS = 0,
+    RETURN_EXIT = 255
+};
 
-bool HandleEvent(SDL_Event *event);
+i32  InitGame(void);
+void ExitGame(void);
+i32  HandleEvent(const SDL_Event *event);
+i32  HandleWindow(const SDL_Event *event);
+i32  HandleKeyDown(const SDL_Event *event);
+
+static int ResizeWindow(SDL_Window *window, int width, int height);
+static int UpdateWindow(SDL_Window *window);
 
 // GLOBAL STUFF FOR NOW;
+SDL_Window *window;
+SDL_Renderer *renderer;
+u16 screen_width = 800;
+u16 screen_height = 600;
+
+static SDL_Texture *texture;
+static void *BitmapMemory;
+static i32 BitmapWidth;
+
 
 int main(void) { 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    u16 screen_width = 800;
-    u16 screen_height = 600;
-    u32 exitval;
-    int retval;
+    i32 exitval;
+    i32 retval;
+    bool quit = false;
+
+    retval = InitGame();
+    if(retval < 0) {
+        exitval = EXIT_FAILURE;
+        goto __EXIT__;
+    }
+
+    while(quit == false)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) 
+        {
+           retval = HandleEvent(&event);
+           if(retval == RETURN_EXIT) 
+           {
+               quit = true;
+           }
+        }
+
+        // RenderWeirdGradient(XOffset, YOffset);
+        // UpdateWindow(window, renderer);
+    }
+
+    exitval = EXIT_SUCCESS;
+__EXIT__:
+     ExitGame();
+     return exitval;
+}
+
+
+i32 InitGame(void)
+{
+    i32 retval;
     int sdl_flags = SDL_INIT_VIDEO | SDL_INIT_JOYSTICK;
     int rnd_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-
-    exitval = EXIT_FAILURE;
 
     retval = SDL_Init(sdl_flags);
     if(retval < 0)
     {
-        SDL_LogError (SDL_LOG_CATEGORY_APPLICATION, "SDL_Init: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
+        SDL_LogError (SDL_LOG_CATEGORY_APPLICATION, 
+                      "%s: SDL_Init(): %s\n", 
+                      __func__, SDL_GetError());
+        return retval;
     }
 
     window = SDL_CreateWindow("Gamepads and viewports",
@@ -50,9 +103,8 @@ int main(void) {
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
                 "%s: SDL_CreateWindow(): %s\n",
-                __func__,
-                SDL_GetError());
-        goto __EXIT__;
+                __func__, SDL_GetError());
+        return RETURN_FAILURE;
     }
 
     renderer = SDL_CreateRenderer(window, -1, rnd_flags);
@@ -62,77 +114,204 @@ int main(void) {
                 "%s: SDL_CreateRenderer(): %s\n",
                 __func__,
                 SDL_GetError());
-        goto __EXIT__;
+        return RETURN_FAILURE;
     }
 
+    return RETURN_SUCCESS;
+}
 
-    bool quit = false;
-    SDL_Event event;
-    while(quit == false)
-    {
-        SDL_WaitEvent(&event);
-        quit = HandleEvent(&event);
-    }
-    exitval = EXIT_SUCCESS;
+
+void ExitGame(void)
+{
     SDL_Log("Exiting\n");
-
-__EXIT__:
     if (renderer)           SDL_DestroyRenderer(renderer);
     if (window)             SDL_DestroyWindow(window);
     SDL_Quit();
-    return exitval;
+    return; 
 }
 
-bool HandleEvent(SDL_Event *event)
+
+i32 HandleEvent(const SDL_Event *event)
 {
-    bool quit;  
+    i32 retval;
     switch(event->type)
     {
-        case SDL_QUIT:
-            SDL_Log("Window close requested\n");
-            quit = true;
-            break;
-
-        case SDL_WINDOWEVENT:
-            static bool isWhite = true;
-            switch(event->window.event)
-            {
-                case SDL_WINDOWEVENT_RESIZED:
-                    SDL_Log("Window Resized (%d, %d)\n", event->window.data1, event->window.data2);
-                    break;
-                case SDL_WINDOWEVENT_EXPOSED:
-                {
-                    SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
-                    SDL_Renderer *renderer = SDL_GetRenderer(window);
-                    SDL_Log("Window exposed!\n");
-                    if(isWhite == true)
-                    {
-                        SDL_Log("White!\n");
-                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                        isWhite = false;
-                    } else {
-                        SDL_Log("Black!\n");
-                        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                        isWhite = true;
-                    }
-                    SDL_RenderClear(renderer);
-                    SDL_RenderPresent(renderer);
-                } break;
-            }
-            break;
-
-        case SDL_KEYDOWN:
-            switch(event->key.keysym.sym)
-            {
-                case SDLK_ESCAPE:
-                case SDLK_q: quit = true; break;
-                case SDLK_RETURN:
-                    if(event->key.keysym.mod & KMOD_LALT) {
-                        SDL_Log("ALT-ENTER pressed\n");
-                    }
-                    break;
-            }
-            break;
+        case SDL_QUIT:          retval = RETURN_EXIT; break;
+        case SDL_WINDOWEVENT:   retval = HandleWindow(event); break;
+        case SDL_KEYDOWN:       retval = HandleKeyDown(event); break;
+        default: break;
     }
-    return quit;
+    return retval;
+}
+
+
+i32 HandleWindow(const SDL_Event *event)
+{
+    switch(event->window.event)
+    {
+        case SDL_WINDOWEVENT_MOVED:
+        {} break;
+
+        case SDL_WINDOWEVENT_RESIZED:
+        { /* handled in SIZE_CHANGED */ } break;
+
+        case SDL_WINDOWEVENT_SIZE_CHANGED:
+        {
+            SDL_Log("%s: Window size changed (%d, %d)\n",
+                    __func__, event->window.data1, event->window.data2);
+
+            SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+            ResizeWindow(window, event->window.data1, event->window.data2);
+        } break;
+
+        case SDL_WINDOWEVENT_FOCUS_GAINED:
+        {
+            SDL_Log("%s: Window focus gained\n", __func__);
+        } break;
+
+        case SDL_WINDOWEVENT_FOCUS_LOST:
+        {
+            SDL_Log("%s: Window focus lost\n", __func__);
+        } break;
+
+        case SDL_WINDOWEVENT_ENTER:
+        {
+            SDL_Log("%s: Mouse focus gained\n", __func__);
+        } break;
+
+        case SDL_WINDOWEVENT_LEAVE:
+        {
+            SDL_Log("%s: Mouse focus lost\n", __func__);
+        } break;
+
+        case SDL_WINDOWEVENT_EXPOSED:
+        {
+            SDL_Log("%s: Window exposed\n", __func__);
+            SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+            UpdateWindow(window);
+        } break;
+
+        case SDL_WINDOWEVENT_CLOSE:
+        {
+            SDL_Log("%s: Window Close requested\n", __func__);
+            return RETURN_EXIT;
+        } break;
+
+        case SDL_WINDOWEVENT_TAKE_FOCUS:
+        {
+            SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+            SDL_RaiseWindow(window);
+        } break;
+
+        default:
+        {
+            SDL_Log("%s: Window event not handled: %d\n", __func__, event->window.event);
+        } break;
+    }
+    return RETURN_SUCCESS;
+}
+
+
+i32 HandleKeyDown(const SDL_Event *event)
+{
+    switch(event->key.keysym.sym)
+    {
+        case SDLK_ESCAPE: return RETURN_EXIT; break;
+        case SDLK_q: return RETURN_EXIT; break;
+        case SDLK_RETURN:
+            if(event->key.keysym.mod & KMOD_LALT) {
+                SDL_Log("ALT-ENTER pressed\n");
+            }
+            break;
+        default: break;
+    }
+    return RETURN_SUCCESS;
+}
+
+static int ResizeWindow(SDL_Window *window, int width, int height)
+{
+    SDL_Renderer *renderer = SDL_GetRenderer(window);
+
+    if(texture)
+    {
+        SDL_DestroyTexture(texture);
+    }
+    texture = SDL_CreateTexture(renderer, 
+                                SDL_PIXELFORMAT_ARGB8888,
+                                SDL_TEXTUREACCESS_STREAMING,
+                                width,
+                                height);
+    if(!texture)
+    {
+        SDL_LogError (SDL_LOG_CATEGORY_APPLICATION, 
+                      "%s: SDL_CreateTexture(): %s\n", 
+                      __func__, SDL_GetError());
+        return RETURN_FAILURE;
+    }
+
+    if(BitmapMemory)
+    {
+        free(BitmapMemory);
+        //munmap(BitmapMemory, width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
+    }
+    // Later benchmark vs mmap
+    BitmapMemory = malloc(width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
+    //BitmapMemory = mmap(NULL,
+    //                    width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888),
+    //                    PROT_READ | PROT_WRITE,
+    //                    MAP_ANONYMOUS | MAP_PRIVATE,
+    //                    -1,
+    //                    0);
+    if(!BitmapMemory)
+    {
+        SDL_LogError (SDL_LOG_CATEGORY_APPLICATION, 
+                      "%s: malloc(BitmapMemory): %s\n", 
+                      __func__, SDL_GetError());
+        return RETURN_FAILURE;
+    }
+    BitmapWidth = width;
+    return RETURN_SUCCESS;
+}
+
+static int UpdateWindow(SDL_Window *window)
+{
+    i32 retval = RETURN_FAILURE;
+
+    if(!texture) {
+        return retval;
+    }
+
+    retval = SDL_UpdateTexture(texture,
+                               NULL, 
+                               BitmapMemory, 
+                               BitmapWidth * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
+
+    if(retval < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: SDL_UpdateTexture(): %s\n",
+                __func__, SDL_GetError());
+        return retval;
+    }
+
+    SDL_Renderer *renderer = SDL_GetRenderer(window);
+    if(!renderer)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: SDL_GetRenderer(): %s\n",
+                __func__, SDL_GetError());
+        return RETURN_FAILURE;
+    }
+
+    retval = SDL_RenderCopy(renderer, texture, NULL, NULL);
+    if(retval < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: SDL_RenderCopy(): %s\n",
+                __func__, SDL_GetError());
+        return retval;
+    }
+
+    SDL_RenderPresent(renderer);
+    return RETURN_SUCCESS;
 }
