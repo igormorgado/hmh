9739081b358f958c7951ccd32e86940c16678b4c
Igor Morgado <morgado.igor@gmail.com>
2020-07-17 00:15:21 -0300

    Chapter 4

--- src/main.c
+++ src/main.c
@@ -24,38 +24,45 @@ enum RETURN_STATUS {
     RETURN_EXIT = 255
 };
 
-i32  InitGame(void);
-void ExitGame(void);
+SDL_Window * InitGame(u16 screen_width, u16 screen_height);
+void ExitGame(SDL_Window *window);
 i32  HandleEvent(const SDL_Event *event);
 i32  HandleWindow(const SDL_Event *event);
 i32  HandleKeyDown(const SDL_Event *event);
 
-static int ResizeWindow(SDL_Window *window, int width, int height);
+static int ResizeWindow(SDL_Window *window, u16 width, u16 height);
 static int UpdateWindow(SDL_Window *window);
+static int RenderWeirdGradient(SDL_Window *window, i16 xoffset, i16 yoffset);
 
 // GLOBAL STUFF FOR NOW;
-SDL_Window *window;
-SDL_Renderer *renderer;
-u16 screen_width = 800;
-u16 screen_height = 600;
 
+// Need to go someewhere.. 
 static SDL_Texture *texture;
 static void *BitmapMemory;
 static i32 BitmapWidth;
+static i32 BitmapHeight;
 
 
-int main(void) { 
+int
+main(void) 
+{ 
     i32 exitval;
     i32 retval;
-    bool quit = false;
 
-    retval = InitGame();
-    if(retval < 0) {
+    SDL_Window *window = InitGame(800, 600);
+    if(!window) {
         exitval = EXIT_FAILURE;
         goto __EXIT__;
     }
 
-    while(quit == false)
+    i32 width;
+    i32 height;
+    f32 XOffset = 0;
+    f32 YOffset = 0;
+    SDL_GetWindowSize(window, &width, &height);
+    ResizeWindow(window, width, height);
+    bool running = true;
+    while(running == true)
     {
         SDL_Event event;
         while(SDL_PollEvent(&event)) 
@@ -63,25 +70,30 @@ int main(void) {
            retval = HandleEvent(&event);
            if(retval == RETURN_EXIT) 
            {
-               quit = true;
+               running = false;
            }
         }
-
-        // RenderWeirdGradient(XOffset, YOffset);
-        // UpdateWindow(window, renderer);
+        RenderWeirdGradient(window, XOffset, YOffset);
+        UpdateWindow(window);
+        ++XOffset;
+        YOffset+=.75;
     }
 
     exitval = EXIT_SUCCESS;
 __EXIT__:
-     ExitGame();
+     ExitGame(window);
      return exitval;
 }
 
 
-i32 InitGame(void)
+SDL_Window *
+InitGame(u16 screen_width, u16 screen_height)
 {
+    // TODO: Assert Window not null
+    // TODO: Assert widht/height < 4096
     i32 retval;
     int sdl_flags = SDL_INIT_VIDEO | SDL_INIT_JOYSTICK;
+    int wnd_flags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
     int rnd_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 
     retval = SDL_Init(sdl_flags);
@@ -90,23 +102,25 @@ i32 InitGame(void)
         SDL_LogError (SDL_LOG_CATEGORY_APPLICATION, 
                       "%s: SDL_Init(): %s\n", 
                       __func__, SDL_GetError());
-        return retval;
+        return NULL;
     }
 
-    window = SDL_CreateWindow("Gamepads and viewports",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            screen_width,
-            screen_height,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Window *window;
+    window = SDL_CreateWindow("HMH",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              screen_width,
+                              screen_height,
+                              wnd_flags);
     if(!window)
     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
                 "%s: SDL_CreateWindow(): %s\n",
                 __func__, SDL_GetError());
-        return RETURN_FAILURE;
+        return NULL;
     }
 
+    SDL_Renderer *renderer;
     renderer = SDL_CreateRenderer(window, -1, rnd_flags);
     if(!renderer)
     {
@@ -114,16 +128,19 @@ i32 InitGame(void)
                 "%s: SDL_CreateRenderer(): %s\n",
                 __func__,
                 SDL_GetError());
-        return RETURN_FAILURE;
+        return NULL;
     }
 
-    return RETURN_SUCCESS;
+    return window;
 }
 
 
-void ExitGame(void)
+void
+ExitGame(SDL_Window *window)
 {
+    // TODO: Assert Window not null
     SDL_Log("Exiting\n");
+    SDL_Renderer *renderer = SDL_GetRenderer(window);
     if (renderer)           SDL_DestroyRenderer(renderer);
     if (window)             SDL_DestroyWindow(window);
     SDL_Quit();
@@ -131,8 +148,10 @@ void ExitGame(void)
 }
 
 
-i32 HandleEvent(const SDL_Event *event)
+i32
+HandleEvent(const SDL_Event *event)
 {
+    // TODO: Assert eevent not null
     i32 retval;
     switch(event->type)
     {
@@ -145,8 +164,10 @@ i32 HandleEvent(const SDL_Event *event)
 }
 
 
-i32 HandleWindow(const SDL_Event *event)
+i32
+HandleWindow(const SDL_Event *event)
 {
+    // TODO: Assert eevent not null
     switch(event->window.event)
     {
         case SDL_WINDOWEVENT_MOVED:
@@ -212,8 +233,10 @@ i32 HandleWindow(const SDL_Event *event)
 }
 
 
-i32 HandleKeyDown(const SDL_Event *event)
+i32
+HandleKeyDown(const SDL_Event *event)
 {
+    // TODO: Assert eevent not null
     switch(event->key.keysym.sym)
     {
         case SDLK_ESCAPE: return RETURN_EXIT; break;
@@ -228,14 +251,26 @@ i32 HandleKeyDown(const SDL_Event *event)
     return RETURN_SUCCESS;
 }
 
-static int ResizeWindow(SDL_Window *window, int width, int height)
-{
-    SDL_Renderer *renderer = SDL_GetRenderer(window);
 
+static int
+ResizeWindow(SDL_Window *window, u16 width, u16 height)
+{
+    // TODO: ASSERT WINDOW not null
+    // TODO: ASSERT 0 < width/height <= 4096
+
+    if(BitmapMemory)
+    {
+        free(BitmapMemory);
+        //munmap(BitmapMemory, width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
+    }
+    
     if(texture)
     {
         SDL_DestroyTexture(texture);
     }
+
+    SDL_Renderer *renderer = SDL_GetRenderer(window);
+    // TODO: GET PIXELFORMAT FROM RENDERER
     texture = SDL_CreateTexture(renderer, 
                                 SDL_PIXELFORMAT_ARGB8888,
                                 SDL_TEXTUREACCESS_STREAMING,
@@ -249,13 +284,11 @@ static int ResizeWindow(SDL_Window *window, int width, int height)
         return RETURN_FAILURE;
     }
 
-    if(BitmapMemory)
-    {
-        free(BitmapMemory);
-        //munmap(BitmapMemory, width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
-    }
     // Later benchmark vs mmap
-    BitmapMemory = malloc(width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
+    // TODO: GET PIXEL FORMAT FROM TEXTURE
+    BitmapWidth = width;
+    BitmapHeight = height;
+    BitmapMemory = malloc(BitmapWidth * BitmapHeight * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888));
     //BitmapMemory = mmap(NULL,
     //                    width * height * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888),
     //                    PROT_READ | PROT_WRITE,
@@ -269,18 +302,23 @@ static int ResizeWindow(SDL_Window *window, int width, int height)
                       __func__, SDL_GetError());
         return RETURN_FAILURE;
     }
-    BitmapWidth = width;
     return RETURN_SUCCESS;
 }
 
-static int UpdateWindow(SDL_Window *window)
-{
-    i32 retval = RETURN_FAILURE;
 
+static int
+UpdateWindow(SDL_Window *window)
+{
+    // TODO: ASSERT WINDOW not null
+    i32 retval = RETURN_FAILURE;
     if(!texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: Texture NULL, nothing to update\n",
+                __func__);
         return retval;
     }
 
+    // TODO: Get Pixel format from texture;
     retval = SDL_UpdateTexture(texture,
                                NULL, 
                                BitmapMemory, 
@@ -313,5 +351,55 @@ static int UpdateWindow(SDL_Window *window)
     }
 
     SDL_RenderPresent(renderer);
+    return RETURN_SUCCESS;
+}
+
+
+static int
+RenderWeirdGradient(SDL_Window *window, i16 blueoffset, i16 greenoffset)
+{
+
+    // TODO: ASSERT WINDOW NOT NULL
+    // TODO: ASSERT BLUEOFFSET GREEN OFFSET < 255
+    i32 retval = RETURN_FAILURE;
+
+    SDL_Renderer *renderer = SDL_GetRenderer(window);
+    if(!renderer)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: SDL_GetRenderer(): %s\n",
+                __func__, SDL_GetError());
+        return retval;
+    }
+
+    SDL_RendererInfo info;
+    retval = SDL_GetRendererInfo(renderer, &info);
+    if(retval < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                "%s: SDL_GetRendererInfo(): %s\n",
+                __func__, SDL_GetError());
+        return retval;
+    }
+
+    i32 width = BitmapWidth;
+    //i32 height = BitmapHeight;
+
+    // TODO: Retrieve the pixel format from texture
+    i32 pitch = width * SDL_BYTESPERPIXEL(SDL_PIXELFORMAT_ARGB8888);
+
+    u8 *row = (u8 *)BitmapMemory;
+    for(int y = 0; y < BitmapHeight; ++y)
+    {
+        u32 *pixel = (u32*)row;
+        for(int x = 0; x < BitmapWidth; ++x)
+        {
+            u8 blue = (x + blueoffset);
+            u8 green = (y + greenoffset);
+            u8 red = 0x00;
+            *pixel++ = ((red << 16) | (green << 8) | blue);
+        }
+        row += pitch;
+    }
     return RETURN_SUCCESS;
 }
